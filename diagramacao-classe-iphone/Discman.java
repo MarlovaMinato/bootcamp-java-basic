3716ac4e8c9c18d1ef143e9cbff88c617e3e23d3
MarlovaMinato <82427141+MarlovaMinato@users.noreply.github.com>
2024-08-13 01:40:17 -0300

    Classe que implementa a interface de reprodutor musical

---
+++ diagramacao-classe-iphone/Discman.java
@@ -0,0 +1,17 @@
+public class Discman implements ReprodutorMusical {
+    
+    @Override
+    public void tocar() {
+        // Implementar a lógica para tocar música
+    }
+
+    @Override
+    public void pausar() {
+        // Implementar a lógica para pausar música
+    }
+
+    @Override
+    public void selecionarMusica() {
+        // Implementar a lógica para selecionar música
+    }
+}
