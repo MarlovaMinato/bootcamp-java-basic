2fa4736dbf058072d52201d39f0afdc17421bacf
MarlovaMinato <82427141+MarlovaMinato@users.noreply.github.com>
2024-08-13 01:43:39 -0300

    Classe que implementa a interface de navegador de internet

---
+++ diagramacao-classe-iphone/Chrome.java
@@ -0,0 +1,19 @@
+public class implements Chrome NavegadorInternet {
+
+    @Override
+    public void exibirPagina() {
+        // Implementar a lógica para exibir página
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        // Implementar a lógica para adicionar nova aba
+    }
+
+    @Override
+    public void atualizarPagina() {
+        // Implementar a lógica para atualizar página
+    }
+}
+    
+}
