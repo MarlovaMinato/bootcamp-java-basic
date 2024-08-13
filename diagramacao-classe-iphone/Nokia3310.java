439955c775a2b17c9c101ed0d664b4332525263f
MarlovaMinato <82427141+MarlovaMinato@users.noreply.github.com>
2024-08-13 01:36:54 -0300

    Classe que implementa a interface telefônica

---
+++ diagramacao-classe-iphone/Nokia3310.java
@@ -0,0 +1,18 @@
+public class Nokia3310 implements AparelhoTelefonico {
+    
+    @Override
+    public void ligar() {
+        // Implementar a lógica para ligar
+    }
+
+    @Override
+    public void atender() {
+        // Implementar a lógica para atender
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        // Implementar a lógica para iniciar correio de voz
+    }
+}
+
