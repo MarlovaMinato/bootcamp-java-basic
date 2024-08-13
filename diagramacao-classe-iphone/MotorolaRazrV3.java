2f1698110f22223f484e1fbc115fa4143a1f39d9
MarlovaMinato <82427141+MarlovaMinato@users.noreply.github.com>
2024-08-13 01:38:00 -0300

    Classe que implementa a interface telefônica

---
+++ diagramacao-classe-iphone/MotorolaRazrV3.java
@@ -0,0 +1,17 @@
+public class MotorolaRazrV3 implements AparelhoTelefonico {
+    
+    @Override
+    public void ligar() {
+        // Implementar a lógica para ligar
+    }
+
+    @Override
+    public void atender() {
+        // Implementar a lógica para atender
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        // Implementar a lógica para iniciar correio de voz
+    }
+}
