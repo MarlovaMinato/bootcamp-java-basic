ec703b0b83958454b32d20997e376274a1a47054
MarlovaMinato <82427141+MarlovaMinato@users.noreply.github.com>
2024-08-13 01:35:29 -0300

    Classe que implementa as interfaces para telefone, navegador, e reprodutor musical

---
+++ diagramacao-classe-iphone/IPhone.java
@@ -0,0 +1,48 @@
+public class IPhone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical {
+    
+    @Override
+    public void tocar() {
+        // Implementar a lógica para tocar música
+    }
+
+    @Override
+    public void pausar() {
+        // Implementar a lógica para pausar música
+    }
+
+    @Override
+    public void selecionarMusica() {
+        // Implementar a lógica para selecionar música
+    }
+
+    @Override
+    public void ligar() {
+        // Implementar a lógica para ligar
+    }
+
+    @Override
+    public void atender() {
+        // Implementar a lógica para atender
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        // Implementar a lógica para iniciar correio de voz
+    }
+
+    @Override
+    public void exibirPagina() {
+        // Implementar a lógica para exibir página
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        // Implementar a lógica para adicionar nova aba
+    }
+
+    @Override
+    public void atualizarPagina() {
+        // Implementar a lógica para atualizar página
+    }
+}
+
