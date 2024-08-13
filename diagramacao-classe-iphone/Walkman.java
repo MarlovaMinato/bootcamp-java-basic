af253cd99ba091daa82f069bb99ccb474771e6c5
MarlovaMinato <82427141+MarlovaMinato@users.noreply.github.com>
2024-08-13 01:39:04 -0300

    Classe que implementa a interface de reprodutor musical

---
+++ diagramacao-classe-iphone/Walkman.java
@@ -0,0 +1,18 @@
+public class Walkman implements ReprodutorMusical {
+    
+    @Override
+    public void tocar() {
+        // Implementar a lógica para tocar música
+    }
+
+    @Override
+    public void pausar() {
+        // Implementar a lógica para pausar música
+    }
+
+    @Override
+    public void selecionarMusica() {
+        // Implementar a lógica para selecionar música
+    }
+}
+
