a5d0ddafbf0800bd6e81d6b59b22465f46b2df06
MarlovaMinato <82427141+MarlovaMinato@users.noreply.github.com>
2024-08-13 01:41:51 -0300

    Classe que implementa a interface de navegador de internet

---
+++ diagramacao-classe-iphone/FireFox.java
@@ -0,0 +1,18 @@
+public class FireFox implements NavegadorInternet {
+    
+    @Override
+    public void exibirPagina() {
+        // Implementar a lógica para exibir página
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        // Implementar a lógica para adicionar nova aba
+    }
+
+    @Override
+    public void atualizarPagina() {
+        // Implementar a lógica para atualizar página
+    }
+}
+
